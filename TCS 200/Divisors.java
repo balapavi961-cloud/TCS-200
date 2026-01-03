195705fd48996529e54bee598259ea25b3587f0e
Balamurugan R <balapavi961@gmail.com>
2026-01-03 22:20:14 +0530

    Add files via upload

---
+++ TCS 200/Divisors.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Divisors {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=2;i<n;i++){
+            System.out.println(i+" ");
+        }
+    }
+}

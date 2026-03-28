05a0fe30a17be64e1c4ab6eee1be55eb2f3a6a5b
Balamurugan R <balapavi961@gmail.com>
2026-03-28 21:44:51 +0530

    Delete TCS 200/BitCount_Pgm.java

--- TCS 200/BitCount_Pgm.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class BitCount_Pgm{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int count = Integer.bitCount(n);
-        System.out.println(count);
-    }
-}
